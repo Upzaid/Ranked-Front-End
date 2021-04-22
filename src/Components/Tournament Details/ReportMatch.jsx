bb2fb867c33d107d4b309844a93aa6e15c05bb8e
Zaid <zaid_lorea@yahoo.com.mx>
2021-04-21 20:36:43 -0500

    Report match functionality added

--- src/Components/Tournament Details/ReportMatch.jsx
+++ src/Components/Tournament Details/ReportMatch.jsx
@@ -1,13 +1,39 @@
 import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom'
 
 function ReportMatch(props){
     // TODO Functionality
+
+    const rankedAPI = process.env.REACT_APP_API_URL
+
+    let [message, setMessage] = useState()
+    let {tournament_id} = useParams()
+
+    async function submitMatch() {
+        const match = {
+            tournament_id: tournament_id,
+            player1: document.getElementById('player1').options[document.getElementById('player1').selectedIndex].text,
+            player2: document.getElementById('player2').options[document.getElementById('player2').selectedIndex].text,
+            p1_wins: document.getElementById('p1wins').value,
+            p2_wins: document.getElementById('p2wins').value
+        }
+        
+        await fetch(`${rankedAPI}/match/submit`,{
+            method: 'POST',
+            body: JSON.stringify(match),
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'ranked-token': localStorage.getItem('ranked-token')
+            }
+        })
+    }
 
     return(
         <div className="content-container">
             <h1>Report Match</h1>
             
-            <form onSubmit={e =>{e.preventDefault()}} classname="">
+            <form onSubmit={e =>{e.preventDefault()}} className="">
                 <label htmlFor="">Player 1: </label>
                 <select name="" id="player1">
                     <option value=""></option>
@@ -28,11 +54,11 @@ function ReportMatch(props){
                 </select>
                 <br/>
                 <label htmlFor="p1wins">Player 1 wins:</label>
-                <input type="number" name="p1wins" id=""/>
+                <input type="number" name="p1wins" id="p1wins" min="0"/>
                 <label htmlFor="p2wins">Player 2 wins:</label>
-                <input type="number" name="p2wins" id=""/>
+                <input type="number" name="p2wins" id="p2wins" min="0"/>
                 <br/>
-                <button className="button">SEND RESULT</button>
+                <button className="button" onClick={()=>{submitMatch()}}>SEND RESULT</button>
             </form>
         </div>
     )
