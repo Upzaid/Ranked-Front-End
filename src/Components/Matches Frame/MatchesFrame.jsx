8462bb58e0bbbd661fe16bcc79145e984c21d815
Zaid <zaid_lorea@yahoo.com.mx>
2021-04-24 23:39:09 -0500

    Added user match history barebones funcionality

--- src/Components/Matches Frame/MatchesFrame.jsx
+++ src/Components/Matches Frame/MatchesFrame.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 
 function MatchesFrame(){
+
+    useEffect(()=>{
+        getMatches()
+    },[])
+
+    const rankedAPI = process.env.REACT_APP_API_URL
+
+    async function getMatches() {
+        const response = await fetch(`${rankedAPI}/match/user/all`,{
+            headers:{
+                'ranked-token': localStorage.getItem('ranked-token')
+            }
+        })
+        console.log(await response.json());
+    }
+
     return(
         <div className='content'>
-            <h1>Matches Frame</h1>
+            <h1>Match History</h1>
         </div>
     )
 }
